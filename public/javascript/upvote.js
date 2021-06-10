d8fddca9888db4ed88e0a13925dbe95c96bf1df1
Anomalous Alpha <blackcarrera@msn.com>
2021-06-09 18:20:38 -0600

    check session login in order to comment, adding comments work, but still, with the fresh snapshot code and copied code for for the upvote button, those do not work, the logout does not work

---
+++ public/javascript/upvote.js
@@ -0,0 +1,12 @@
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    console.log(id);
+    console.log('button clicked');
+}
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+
